da47a95bf2f20add0f6cbe7aedbefa33dcb4bfb0
Tobias Zucali <tobias@zucali.at>
2016-02-24 21:44:45 +0100

    add draft.js

--- app/App.js
+++ app/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
            	<ul role="nav">
 	            <li><Link to="/one" activeStyle={{ color: 'red' }}>One</Link></li>
 	            <li><Link to="/one/bar" activeStyle={{ color: 'red' }}>One/bar</Link></li>
-	            <li><Link to="/two" activeStyle={{ color: 'red' }}>Two</Link></li>
+	            <li><Link to="/two" activeStyle={{ color: 'red' }}>Two (with draft.js)</Link></li>
 	            <li><Link to="/two/bar" activeStyle={{ color: 'red' }}>Two/bar</Link></li>
 	        </ul>
         </div>
@@ -29,6 +29,7 @@ class App extends React.Component {
  * protoTypes
  */
 App.propTypes =  {
+  /** @type {Object[]} some prop */
   optionalArray: React.PropTypes.array,
   optionalBool: React.PropTypes.bool,
   optionalFunc: React.PropTypes.func,
--- app/Two.js
+++ app/Two.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Intl from 'intl';
+import Editor from './Editor';
 
 class Two extends React.Component {
     render() {
         const foo = (this.props.params && this.props.params.foo) ? `(${this.props.params.foo})` : null;
+
         return (
             <div>
                 Two { foo }
+                <Editor />
             </div>
         );
     }
